420f48a814366dd06b366b849e56ce936f8be3db
med-mokhtar-nasrali <mokhtarbackup03@gmail.com>
2025-01-06 13:36:14 +0100

    FirstJavaPrograrm

---
+++ Week_1/Day_1/Practice/FirstJavaPrograrm/FirstJavaPrograrm.java
@@ -0,0 +1,8 @@
+public class FirstJavaPrograrm {
+
+    public static void main(String[] args) {
+        System.out.println("My Name is Med Mokhtar Nasrali");
+        System.out.println("I am 22 years old");
+        System.out.println("My hometown is Tunis, Tunisia");
+    }
+}
